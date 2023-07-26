0b7544c2a26086b55e256e984860454f7bf26ef5
Daniel <94169588+dglezalbo@users.noreply.github.com>
2023-07-26 13:40:24 +0200

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,5 @@
-const navbar = document.querySelector(".navbar");
+const navbar = document.querySelector(".navbar-container");
+const menuContainer = document.querySelector(".menu-container");
 
 window.addEventListener("scroll", function () {
   const scrollPosition = window.scrollY;
@@ -6,5 +7,20 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("hidden");
   } else {
     navbar.classList.add("hidden");
+    menuContainer.style.top = `-200px`;
   }
 });
+
+const menuButton = document.querySelector(".menu-button");
+const openMenu = function () {
+  menuContainer.style.display = `flex`;
+  menuContainer.style.top = `0px`;
+};
+const closeMenu = function () {
+  menuContainer.style.display = `none`;
+  menuContainer.style.top = `-145px`;
+};
+
+menuButton.addEventListener("click", openMenu);
+
+window.addEventListener("scroll", closeMenu);
