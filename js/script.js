b62dce806f89379893ceb8a8fe7fc2fe16201666
Daniel <danielglezalbo@gmail.com>
2024-09-26 16:47:07 +0200

    update: improved stack section in /about

--- js/script.js
+++ js/script.js
@@ -75,3 +75,74 @@ document.addEventListener("DOMContentLoaded", function () {
   // Initialize carousel
   updateCarousel();
 });
+
+// draggable thingy
+
+const els = document.querySelectorAll(".mydiv");
+els.forEach((name) => {
+  dragElement(name);
+});
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  const originalTransform = elmnt.style.transform; // Store original transform
+
+  elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+
+    // Increase size while dragging
+    elmnt.style.transform = originalTransform + " scale(1.1)";
+    elmnt.style.transition = "none"; // Disable transition
+    elmnt.style.zIndex = zIndexCounter++; // Set zIndex and increment
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    // Get the bounding rectangle of the macbook
+    const macbook = document.querySelector(".macbook");
+    const macbookRect = macbook.getBoundingClientRect();
+
+    // Calculate new position
+    const newTop = elmnt.offsetTop - pos2;
+    const newLeft = elmnt.offsetLeft - pos1;
+
+    // Check if the new position is within the bounds of the macbook
+    if (
+      newTop >= 0 &&
+      newTop + elmnt.offsetHeight <= macbookRect.height &&
+      newLeft >= 0 &&
+      newLeft + elmnt.offsetWidth <= macbookRect.width
+    ) {
+      elmnt.style.top = newTop + "px";
+      elmnt.style.left = newLeft + "px";
+    }
+  }
+
+  function closeDragElement() {
+    // Reset size when dragging ends
+    elmnt.style.transform =
+      "scale(1) rotate(" + (Math.random() * 80 - 40) + "deg)"; // Restore original transform with random rotation
+    elmnt.style.transition = "0.2s ease-in-out"; // Enable transition
+
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+let zIndexCounter = 10; // Initialize zIndex counter
